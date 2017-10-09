20d30846539667ce312bc12b800003b18fe7c90e
zhouhongfu <zhouhongfu@wanglibank.com>
2017-10-09 10:58:44 +0800

    index

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -10,4 +10,11 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     //
+    public function  __construct()
+    {
+    }
+    public  function  index(){
+
+        return view('admin.home.index');
+    }
 }
